5749b43d49ae34d996ba8376104b67db27df8155
Artem Fomin <anandakirtan@mail.ru>
2025-03-29 20:29:23 +0300

    name all blood constants

--- original/blood-constants.mjs
+++ original/blood-constants.mjs
@@ -1,31 +1,52 @@
 var kAluminum = 'Алюминий';
 var kBoron = 'Бор';
-// var kAluminum = 'Железо';
-// var kAluminum = 'Кадмий';
-// var kAluminum = 'Калий';
-// var kAluminum = 'Кальций';
-// var kAluminum = 'Литий';
-// var kAluminum = 'Магний';
-// var kAluminum = 'Марганец';
-// var kAluminum = 'Медь';
-// var kAluminum = 'Молибден';
-// var kAluminum = 'Мышьяк';
-// var kAluminum = 'Натрий';
-// var kAluminum = 'Никель';
-// var kAluminum = 'Ртуть';
-// var kAluminum = 'Свинец';
-// var kAluminum = 'Селен';
-// var kAluminum = 'Хром';
-// var kAluminum = 'Цинк';
-// var kAluminum = 'Кремний';
-// var kAluminum = 'Кобальт';
-// var kAluminum = 'Титан';
-// var kAluminum = 'Сурьма';
+var kIron = 'Железо';
+var kCadmium = 'Кадмий';
+var kPotassium = 'Калий';
+var kCalcium = 'Кальций';
+var kLithium = 'Литий';
+var kMagnesium = 'Магний';
+var kManganese = 'Марганец';
+var kCopper = 'Медь';
+var kMolybdenum = 'Молибден';
+var kArsenic = 'Мышьяк';
+var kSodium = 'Натрий';
+var kNickel = 'Никель';
+var kMercury = 'Ртуть';
+var kLead = 'Свинец';
+var kSelenium = 'Селен';
+var kChromium = 'Хром';
+var kZinc = 'Цинк';
+var kSilicon = 'Кремний';
+var kCobalt = 'Кобальт';
+var kTitanium = 'Титан';
+var kAntimony = 'Сурьма';
 
 types = [kAluminum, kBoron]
 
+// these are mostly random, will need to set real values later
 var healthy_ranges = new Map([
     [kAluminum, [2.0, 390.0]],
     [kBoron, [6.0, 100.0]],
-    // ...
+    [kIron, [10.0, 100.0]],
+    [kCadmium, [0.0, 0.0]],
+    [kPotassium, [3.5, 5.5]],
+    [kCalcium, [2.25, 2.75]],
+    [kLithium, [0.8, 1.2]],
+    [kMagnesium, [0.8, 1.2]],
+    [kManganese, [0.0, 0.0]],
+    [kCopper, [0.0, 0.0]],
+    [kMolybdenum, [0.0, 0.0]],
+    [kArsenic, [0.0, 0.0]],
+    [kSodium, [135.0, 145.0]],
+    [kNickel, [0.0, 0.0]],
+    [kMercury, [0.0, 0.0]],
+    [kLead, [0.0, 0.0]],
+    [kSelenium, [0.0, 0.0]],
+    [kChromium, [0.0, 0.0]],
+    [kZinc, [0.0, 0.0]],
+    [kSilicon, [0.0, 0.0]],
+    [kCobalt, [0.0, 0.0]],
+    [kTitanium, [0.0, 0.0]],
+    [kAntimony, [0.0, 0.0]],
 ]);
